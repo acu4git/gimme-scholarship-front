e1101c4a08ce7cd7ea75eff5bc2c44c8166c9944
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-05-17 23:33:06 +0900

    ログインと新規登録へのページ遷移をリダイレクトによって行う

--- src/components/header/user_status.tsx
+++ src/components/header/user_status.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import UserButton from "@/components/header/user_button";
-import { SignInButton, SignUpButton, useUser } from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
 
 const UserStatus = () => {
   const { user } = useUser();
@@ -10,24 +11,22 @@ const UserStatus = () => {
   if (!user)
     return (
       <div className="flex gap-3 px-3">
-        <SignInButton mode="modal">
-          <Button
-            variant="outline"
-            size="lg"
-            className="bg-white text-blue-500 border border-blue-500 hover:text-blue-700 hover:cursor-pointer"
-          >
-            ログイン
-          </Button>
-        </SignInButton>
-        <SignUpButton mode="modal">
-          <Button
-            variant="outline"
-            size="lg"
-            className="text-white bg-emerald-300 hover:bg-emerald-500 hover:text-accent hover:cursor-pointer"
-          >
-            新規登録
-          </Button>
-        </SignUpButton>
+        <Button
+          onClick={() => redirect("/sign-in")}
+          variant="outline"
+          size="lg"
+          className="bg-white text-blue-500 border border-blue-500 hover:text-blue-700 hover:cursor-pointer"
+        >
+          ログイン
+        </Button>
+        <Button
+          onClick={() => redirect("/sign-up")}
+          variant="outline"
+          size="lg"
+          className="text-white bg-emerald-300 hover:bg-emerald-500 hover:text-accent hover:cursor-pointer"
+        >
+          新規登録
+        </Button>
       </div>
     );
 
