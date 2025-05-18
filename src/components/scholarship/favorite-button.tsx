ccf540c0c45f727326bdef317971a4397896f77e
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-05-19 03:57:13 +0900

    未使用コードの削除

--- src/components/scholarship/favorite-button.tsx
+++ src/components/scholarship/favorite-button.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button";
 
 const FavoriteButton = () => {
-  const handleClick = () => {};
   return (
     <Button className="bg-white text-black hover:bg-slate-100 hover:cursor-pointer">
       ☆
