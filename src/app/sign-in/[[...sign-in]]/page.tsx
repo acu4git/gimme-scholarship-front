4fe6ce83d00278a2fd90f304fc93dc0b1fd84aea
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-05-19 04:30:38 +0900

    ログイン/サインアップページ間の遷移設定

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function SignInPage() {
   return (
     <div className="h-[70vh] flex justify-center items-center">
-      <SignIn />
+      <SignIn signUpUrl="/sign-up" />
     </div>
   );
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
   return (
     <div className="h-[70vh] flex justify-center items-center">
-      <SignUp />
+      <SignUp signInUrl="/sign-in" />
     </div>
   );
 }
