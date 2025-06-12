46cae7cb5b2cca1f788c11eddbf1a51ad522f4ed
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-06-13 00:50:47 +0900

    簡易的なユーザーページ

--- src/app/users/[userId]/page.tsx
+++ src/app/users/[userId]/page.tsx
@@ -1,17 +1,49 @@
 "use client";
 
 import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
 
 const UserPage = () => {
-  const { user } = useUser();
+  const { isLoaded, isSignedIn, user } = useUser();
 
-  if (!user) {
-    <div>Error: Not Signed in.</div>;
+  if (!isLoaded) {
+    return (
+      <div className="h-[90vh] flex justify-center items-center">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (!isSignedIn) {
+    return (
+      <>
+        <div className="h-[90vh] flex flex-row justify-center items-center gap-2">
+          <label>Error:</label>
+          <p>Not signed in</p>
+        </div>
+      </>
+    );
   }
 
   return (
-    <div className="h-[90vh] flex justify-center items-center">
-      <div></div>
+    <div className="h-[90vh] flex flex-col justify-center items-center">
+      <div>
+        <Image src={user.imageUrl} alt="User Icon" width={400} height={400} />
+      </div>
+      <div className="flex flex-col">
+        <div className="flex flex-row gap-2">
+          <label>User ID:</label>
+          <p>{user.id}</p>
+        </div>
+        <div className="flex flex-row gap-2">
+          <label>Username:</label>
+          <p>{user.fullName}</p>
+        </div>
+        <div className="flex flex-row gap-2">
+          <label>Email:</label>
+          <p>{user.emailAddresses[0].emailAddress}</p>
+        </div>
+      </div>
     </div>
   );
 };
