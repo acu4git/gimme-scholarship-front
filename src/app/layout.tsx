76fe04502cca0998a74d1cf48953c7f8f8a9fb2f
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-06-15 04:38:21 +0900

    faviconとogpの設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,13 +7,32 @@ import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
   title: "KITクレクレ奨学金",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", rel: "icon", type: "image/x-icon" },
+      {
+        url: "/favicon-16x16.png",
+        rel: "icon",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        url: "/favicon-32x32.png",
+        rel: "icon",
+        sizes: "32x32",
+        type: "image/png",
+      },
+    ],
+    apple: [{ url: "/apple-touch-icon.png", rel: "apple-touch-icon" }],
+  },
+  manifest: "/manifest.json",
   openGraph: {
     title: "KITクレクレ奨学金",
     description:
       "KITクレクレ奨学金は、京都工芸繊維大学の学生を対象とした奨学金を検索できるサイトです。効率良く奨学金を探して時間を有効活用しよう！",
     images: [
       {
-        url: "/ogp/money_eyes.svg",
+        url: "/ogp/image.svg",
         width: 500,
         height: 500,
       },
