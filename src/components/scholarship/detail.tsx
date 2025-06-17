1faa9495ce5b8a92a1e1990fcd94b27c902f39ee
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-06-18 01:51:45 +0900

    詳細の表示パターンの修正と大学HPの文字列をリンク化

--- src/components/scholarship/detail.tsx
+++ src/components/scholarship/detail.tsx
@@ -11,6 +11,10 @@ import {
 import { Scholarship } from "@/types/scholarship/scholarship";
 
 const ScholarshipDetail = ({ detail }: { detail: Scholarship }) => {
+  const linkText = "大学HP";
+  const linkUrl =
+    "https://www.kit.ac.jp/campus_index/life_fee/scholarship/minkanscholarship/";
+  const parts = detail.remark.split(linkText);
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -24,22 +28,22 @@ const ScholarshipDetail = ({ detail }: { detail: Scholarship }) => {
         </DialogHeader>
         <div className="overflow-scroll whitespace-pre-line grid grid-cols-1 sm:overflow-auto sm:grid-cols-2 lg:grid-cols-3 text-xs gap-2">
           <div className="border rounded-2xl col-span-1">
-            <p className="font-bold text-xs pl-2 pt-2 ml-2 underline">金額</p>
-            <p className="p-2">{detail.amount_detail}</p>
+            <p className="font-bold text-xs pl-2 pt-2 ml-2 underline">対象</p>
+            <p className="p-2">{detail.target_detail}</p>
           </div>
           <div className="border rounded-2xl col-span-1">
             <p className="font-bold text-xs pl-2 pt-2 ml-2 underline">形式</p>
             <p className="p-2">{detail.type_detail}</p>
           </div>
           <div className="border rounded-2xl col-span-1">
+            <p className="font-bold text-xs pl-2 pt-2 ml-2 underline">金額</p>
+            <p className="p-2">{detail.amount_detail}</p>
+          </div>
+          <div className="border rounded-2xl col-span-1">
             <p className="font-bold text-xs pl-2 pt-2 ml-2 underline">定員</p>
             <p className="p-2">
               {detail.capacity_detail == "" ? "不明" : detail.capacity_detail}
             </p>
-          </div>
-          <div className="border rounded-2xl col-span-1">
-            <p className="font-bold text-xs pl-2 pt-2 ml-2 underline">対象</p>
-            <p className="p-2">{detail.target_detail}</p>
           </div>
           <div className="border rounded-2xl col-span-1">
             <p className="font-bold text-xs pl-2 pt-2 ml-2 underline">住所</p>
@@ -61,7 +65,24 @@ const ScholarshipDetail = ({ detail }: { detail: Scholarship }) => {
           </div>
           <div className="border rounded-2xl col-span-1">
             <p className="font-bold text-xs pl-2 pt-2 ml-2 underline">備考</p>
-            <p className="p-2">{detail.remark}</p>
+            {/* <p className="p-2">{detail.remark}</p> */}
+            <p className="p-2">
+              {parts.map((part, index) => (
+                <span key={index}>
+                  {part}
+                  {index < parts.length - 1 && (
+                    <a
+                      href={linkUrl}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-blue-500 underline"
+                    >
+                      {linkText}
+                    </a>
+                  )}
+                </span>
+              ))}
+            </p>
           </div>
         </div>
         <DialogFooter className="sm:justify-center">
