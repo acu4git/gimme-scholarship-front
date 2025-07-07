3014477eda76d3a91615019020270c2c4e683db8
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-07-07 15:58:19 +0900

    eslintの設定を一部削除

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,13 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      // 'any'の使用をエラーとして検出する
-      "@typescript-eslint/no-explicit-any": "error",
-      // 他に追加したいルールがあればここへ記述
-    },
-  },
 ];
 
 export default eslintConfig;
